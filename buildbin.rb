54f50c57a103b0e94cd2503f8ee5b6007332fce3
Albert Choi <albert.choi@gmail.com>
2017-03-07 15:57:40 -0800

    fix typo

--- buildbin.rb
+++ buildbin.rb
@@ -17,7 +17,7 @@ ospairs = [
 
 binaries = [
   ['natip', 'bin/natip/main.go'],
-  ['baremetal-info', 'bin/natip/main.go'],
+  ['baremetal-info', 'bin/baremetal-info/main.go'],
   ['apiv1', 'bin/apiv1/main.go'],
   ['apiv2', 'bin/apiv2/main.go'],
 ]
@@ -27,8 +27,9 @@ binaries.each do |basename, path|
   ospairs.each do |os, arch, extension|
     cmd = "docker run --rm -it " +
           "-e GOOS=#{os} -e GOARCH=#{arch} " +
-          "-v #{pwd}/bin:/go/src/#{gopkg}/bin " + 
-          "-v #{pwd}/../../CenturyLinkCloud/clc-sdk:/go/src/#{gopkg} " + 
+          "-v #{pwd}/bin:/go/src/#{gopkg}/bin " +
+          "-v #{pwd}/dist:/go/src/#{gopkg}/dist " +
+          "-v #{pwd}/../../CenturyLinkCloud/clc-sdk:/go/src/#{gopkg} " +
           "-w /go/src/#{gopkg} " +
           "golang:1.7 go build -o #{dist}/#{basename}#{extension} #{path}"
     puts "BUILD: #{cmd}"
